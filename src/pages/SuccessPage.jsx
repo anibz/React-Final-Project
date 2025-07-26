e0e7227dcd07a384c84e9fe8d12e01076e99d140
Ani Bzishvili <anibzishvili5@gmail.com>
2025-07-26 15:14:08 +0400

    success page's final update

--- src/pages/SuccessPage.jsx
+++ src/pages/SuccessPage.jsx
@@ -10,7 +10,82 @@ import student_card4 from "../assets/student_card4.png";
 function SuccessPage() {
   const [graduates, setGraduates] = useState([]);
   const [loading, setLoading] = useState(true);
+
   const [showMoreCard, setShowMoreCard] = useState(false);
+
+  const womenInAIStudents = [
+    {
+      id: "1",
+      program: "პროგრამა- Women in Ai",
+      title: "წარმატებული სტუდენტი",
+      name: "სალი გოგიშვილი",
+      imageUrl: student_card1,
+    },
+    {
+      id: "2",
+      program: "პროგრამა- Women in Ai",
+      title: "წარმატებული სტუდენტი",
+      name: "ელენე ცქიტიშვილი",
+      imageUrl: student_card2,
+    },
+    {
+      id: "3",
+      program: "პროგრამა- Women in Ai",
+      title: "წარმატებული სტუდენტი",
+      name: "ნენსი ნენსი",
+      imageUrl: student_card2,
+    },
+    {
+      id: "4",
+      program: "პროგრამა- Women in Ai",
+      title: "წარმატებული სტუდენტი",
+      name: "სალომე მოსიავა",
+      imageUrl: student_card4,
+    },
+
+    {
+      id: "5",
+      program: "პროგრამა- Women in Ai",
+      title: "წარმატებული სტუდენტი",
+      name: "სალი გოგიშვილი",
+      imageUrl: student_card2,
+    },
+    {
+      id: "6",
+      program: "პროგრამა- Women in Ai",
+      title: "წარმატებული სტუდენტი",
+      name: "სალი გოგიშვილი",
+      imageUrl: student_card2,
+    },
+    {
+      id: "7",
+      program: "პროგრამა- Women in Ai",
+      title: "წარმატებული სტუდენტი",
+      name: "სალი გოგიშვილი",
+      imageUrl: student_card2,
+    },
+    {
+      id: "8",
+      program: "პროგრამა- Women in Ai",
+      title: "წარმატებული სტუდენტი",
+      name: "სალი გოგიშვილი",
+      imageUrl: student_card2,
+    },
+    {
+      id: "9",
+      program: "პროგრამა- Women in Ai",
+      title: "წარმატებული სტუდენტი",
+      name: "სალი გოგიშვილი",
+      imageUrl: student_card2,
+    },
+    {
+      id: "10",
+      program: "პროგრამა- Women in Ai",
+      title: "წარმატებული სტუდენტი",
+      name: "სალი გოგიშვილი",
+      imageUrl: student_card2,
+    },
+  ];
 
   useEffect(() => {
     axios
@@ -44,97 +119,31 @@ function SuccessPage() {
           <h2>WOMEN IN AI პროგრამის სტუდენტები</h2>
 
           <div className="student-cards">
-            <div className="card1">
-              <img src={student_card1} alt="student photo" />
-              <div className="card-text">
-                <h4 className="card-title">პროგრამა- Women in Ai</h4>
-                <h5>წარმატებული სტუდენტი</h5>
-                <h5>სალი გოგიშვილი</h5>
+            {(showMoreCard
+              ? womenInAIStudents
+              : womenInAIStudents.slice(0, 4)
+            ).map((student, index) => (
+              <div key={student.id} className="card">
+                <img src={student.imageUrl} alt="student photo" />
+                <div className="card-text">
+                  <h4 className="card-title">{student.program}</h4>
+                  <div className="student-info">
+                    <h5>{student.title}</h5>
+                    <h5>{student.name}</h5>
+                  </div>
+                </div>
               </div>
-            </div>
-            <div className="card2">
-              <img src={student_card2} alt="student photo" />
-              <div className="card-text">
-                <h4 className="card-title">პროგრამა- Women in Ai</h4>
-                <h5>წარმატებული სტუდენტი</h5>
-                <h5>ელენე ცქიტიშვილი</h5>
-              </div>
-            </div>
-            <div className="card3">
-              <img src={student_card2} alt="student photo" />
-              <div className="card-text">
-                <h4 className="card-title">პროგრამა- Women in Ai</h4>
-                <h5>წარმატებული სტუდენტი</h5>
-                <h5>ნენსი ნენსი</h5>
-              </div>
-            </div>
-            <div className="card4">
-              <img src={student_card4} alt="student photo" />
-              <div className="card-text">
-                <h4 className="card-title">პროგრამა- Women in Ai</h4>
-                <h5>წარმატებული სტუდენტი</h5>
-                <h5>სალომე მოსიავა</h5>
-              </div>
-            </div>
-            {showMoreCard && (
-              <>
-                <div className="card5">
-                  <img src={student_card4} alt="student photo" />
-                  <div className="card-text">
-                    <h4 className="card-title">პროგრამა- Women in Ai</h4>
-                    <h5>წარმატებული სტუდენტი</h5>
-                    <h5>სალომე მოსიავა</h5>
-                  </div>
-                </div>
-                <div className="card6">
-                  <img src={student_card4} alt="student photo" />
-                  <div className="card-text">
-                    <h4 className="card-title">პროგრამა- Women in Ai</h4>
-                    <h5>წარმატებული სტუდენტი</h5>
-                    <h5>სალომე მოსიავა</h5>
-                  </div>
-                </div>
-                <div className="card7">
-                  <img src={student_card4} alt="student photo" />
-                  <div className="card-text">
-                    <h4 className="card-title">პროგრამა- Women in Ai</h4>
-                    <h5>წარმატებული სტუდენტი</h5>
-                    <h5>სალომე მოსიავა</h5>
-                  </div>
-                </div>
-                <div className="card8">
-                  <img src={student_card4} alt="student photo" />
-                  <div className="card-text">
-                    <h4 className="card-title">პროგრამა- Women in Ai</h4>
-                    <h5>წარმატებული სტუდენტი</h5>
-                    <h5>სალომე მოსიავა</h5>
-                  </div>
-                </div>
-                <div className="card9">
-                  <img src={student_card4} alt="student photo" />
-                  <div className="card-text">
-                    <h4 className="card-title">პროგრამა- Women in Ai</h4>
-                    <h5>წარმატებული სტუდენტი</h5>
-                    <h5>სალომე მოსიავა</h5>
-                  </div>
-                </div>
-                <div className="card10">
-                  <img src={student_card4} alt="student photo" />
-                  <div className="card-text">
-                    <h4 className="card-title">პროგრამა- Women in Ai</h4>
-                    <h5>წარმატებული სტუდენტი</h5>
-                    <h5>სალომე მოსიავა</h5>
-                  </div>
-                </div>
-              </>
-            )}
+            ))}
           </div>
-          <h4
-            className="show-more-card"
-            onClick={() => setShowMoreCard(!showMoreCard)}
-          >
-            გაიცანი, ყველა წარმატებული სტუდენტი...
-          </h4>
+
+          {womenInAIStudents.length > 4 && (
+            <h4
+              className="show-more-card"
+              onClick={() => setShowMoreCard(!showMoreCard)}
+            >
+              გაიცანი, ყველა წარმატებული სტუდენტი...
+            </h4>
+          )}
         </div>
 
         <div className="student-list-container">
