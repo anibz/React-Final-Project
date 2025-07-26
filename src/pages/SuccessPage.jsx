9e715ae540d98fda6b3cfaa7a48181224f3744b2
Ani Bzishvili <anibzishvili5@gmail.com>
2025-07-26 14:38:17 +0400

    success page done

--- src/pages/SuccessPage.jsx
+++ src/pages/SuccessPage.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import title_background from "../assets/title_background.png";
-
 import { useEffect, useState } from "react";
 import axios from "axios";
+
+import student_card1 from "../assets/student_card1.png";
+import student_card2 from "../assets/student_card2.png";
+import student_card4 from "../assets/student_card4.png";
 
 function SuccessPage() {
   const [graduates, setGraduates] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [showMoreCard, setShowMoreCard] = useState(false);
 
   useEffect(() => {
     axios
@@ -16,13 +20,13 @@ function SuccessPage() {
         setLoading(false);
       })
       .catch((error) => {
-        console.error("Error fetching graduates:", error);
+        console.error("შეცდომა");
         setLoading(false);
       });
   }, []);
 
   if (loading) {
-    return <div>ჩატვირთვა...</div>;
+    return <div>იტვირთება,გთხოვთ დაელოდოთ...</div>;
   }
 
   return (
@@ -35,15 +39,121 @@ function SuccessPage() {
         </div>
       </div>
 
-      <ul>
-        {graduates.map((graduate) => (
-          <li key={graduate.id}>
-            <a href={graduate.link}>
-              {graduate.name} - {graduate.position} | {graduate.company}
-            </a>
-          </li>
-        ))}
-      </ul>
+      <section className="success-section">
+        <div className="student-card-container">
+          <h2>WOMEN IN AI პროგრამის სტუდენტები</h2>
+
+          <div className="student-cards">
+            <div className="card1">
+              <img src={student_card1} alt="student photo" />
+              <div className="card-text">
+                <h4 className="card-title">პროგრამა- Women in Ai</h4>
+                <h5>წარმატებული სტუდენტი</h5>
+                <h5>სალი გოგიშვილი</h5>
+              </div>
+            </div>
+            <div className="card2">
+              <img src={student_card2} alt="student photo" />
+              <div className="card-text">
+                <h4 className="card-title">პროგრამა- Women in Ai</h4>
+                <h5>წარმატებული სტუდენტი</h5>
+                <h5>ელენე ცქიტიშვილი</h5>
+              </div>
+            </div>
+            <div className="card3">
+              <img src={student_card2} alt="student photo" />
+              <div className="card-text">
+                <h4 className="card-title">პროგრამა- Women in Ai</h4>
+                <h5>წარმატებული სტუდენტი</h5>
+                <h5>ნენსი ნენსი</h5>
+              </div>
+            </div>
+            <div className="card4">
+              <img src={student_card4} alt="student photo" />
+              <div className="card-text">
+                <h4 className="card-title">პროგრამა- Women in Ai</h4>
+                <h5>წარმატებული სტუდენტი</h5>
+                <h5>სალომე მოსიავა</h5>
+              </div>
+            </div>
+            {showMoreCard && (
+              <>
+                <div className="card5">
+                  <img src={student_card4} alt="student photo" />
+                  <div className="card-text">
+                    <h4 className="card-title">პროგრამა- Women in Ai</h4>
+                    <h5>წარმატებული სტუდენტი</h5>
+                    <h5>სალომე მოსიავა</h5>
+                  </div>
+                </div>
+                <div className="card6">
+                  <img src={student_card4} alt="student photo" />
+                  <div className="card-text">
+                    <h4 className="card-title">პროგრამა- Women in Ai</h4>
+                    <h5>წარმატებული სტუდენტი</h5>
+                    <h5>სალომე მოსიავა</h5>
+                  </div>
+                </div>
+                <div className="card7">
+                  <img src={student_card4} alt="student photo" />
+                  <div className="card-text">
+                    <h4 className="card-title">პროგრამა- Women in Ai</h4>
+                    <h5>წარმატებული სტუდენტი</h5>
+                    <h5>სალომე მოსიავა</h5>
+                  </div>
+                </div>
+                <div className="card8">
+                  <img src={student_card4} alt="student photo" />
+                  <div className="card-text">
+                    <h4 className="card-title">პროგრამა- Women in Ai</h4>
+                    <h5>წარმატებული სტუდენტი</h5>
+                    <h5>სალომე მოსიავა</h5>
+                  </div>
+                </div>
+                <div className="card9">
+                  <img src={student_card4} alt="student photo" />
+                  <div className="card-text">
+                    <h4 className="card-title">პროგრამა- Women in Ai</h4>
+                    <h5>წარმატებული სტუდენტი</h5>
+                    <h5>სალომე მოსიავა</h5>
+                  </div>
+                </div>
+                <div className="card10">
+                  <img src={student_card4} alt="student photo" />
+                  <div className="card-text">
+                    <h4 className="card-title">პროგრამა- Women in Ai</h4>
+                    <h5>წარმატებული სტუდენტი</h5>
+                    <h5>სალომე მოსიავა</h5>
+                  </div>
+                </div>
+              </>
+            )}
+          </div>
+          <h4
+            className="show-more-card"
+            onClick={() => setShowMoreCard(!showMoreCard)}
+          >
+            გაიცანი, ყველა წარმატებული სტუდენტი...
+          </h4>
+        </div>
+
+        <div className="student-list-container">
+          <h2>
+            კომპიუტერული მეცნიერებისა და ხელოვნური ინტელექტის ფაკულტეტის
+            კურსდამთავრებულთა წარმატება
+          </h2>
+          <ul className="student-list">
+            {graduates.map((graduate) => (
+              <li key={graduate.id}>
+                <a href={graduate.link}>
+                  {graduate.name} - <strong>{graduate.position}</strong> |{" "}
+                  {graduate.company}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </section>
     </>
   );
 }
