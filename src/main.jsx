18ccad09c1538df61db4fddc544da8c65940e514
Ani Bzishvili <anibzishvili5@gmail.com>
2025-07-30 21:30:03 +0400

    homepage is homepaging

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client";
 import "./index.scss";
 import { BrowserRouter } from "react-router-dom";
 import Router from "./router.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,6 +5,15 @@ import success2 from "../assets/success2.png";
 import success3 from "../assets/success3.png";
 import success4 from "../assets/success4.png";
 
+import women1 from "../assets/women1.png";
+import women2 from "../assets/women2.png";
+import women3 from "../assets/women3.png";
+import women4 from "../assets/women4.png";
+
+import carousel1 from "../assets/carousel1.png";
+import carousel2 from "../assets/carousel2.png";
+import carousel3 from "../assets/carousel3.png";
+
 import { useEffect, useState } from "react";
 import axios from "axios";
 
@@ -20,7 +29,45 @@ function HomePage() {
 
   return (
     <div className="homePage">
-      <div className="carusel"></div>
+      <div className="carusel">
+        <div id="carouselExample" class="carousel slide">
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              <img src={carousel1} class="d-block w-100" alt="carousel photo" />
+            </div>
+            <div className="carousel-item">
+              <img src={carousel2} class="d-block w-100" alt="carousel photo" />
+            </div>
+            <div className="carousel-item">
+              <img src={carousel3} class="d-block w-100" alt="carousel photo" />
+            </div>
+          </div>
+          <button
+            className="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselExample"
+            data-bs-slide="prev"
+          >
+            <span
+              className="carousel-control-prev-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button
+            className="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselExample"
+            data-bs-slide="next"
+          >
+            <span
+              className="carousel-control-next-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div>
+      </div>
 
       <div className="ai-section">
         <div className="title">
@@ -120,7 +167,37 @@ function HomePage() {
           <div className="line"></div>
         </div>
         <div className="card-container">
-          <div className="card1"></div>
+          <div className="card1 card">
+            <img src={women1} alt="card photo" />
+            <p>
+              1500 ქალის გადამზადება ტექნოლოგიებში - უმსხვილესი პროექტი BTU-ში
+              USAID-თან პარტნიორობით ხორციელდება;
+            </p>
+          </div>
+          <div className="card2 card">
+            <img src={women2} alt="card photo" />
+            <p>
+              Women in AI-ის ფარგლებში, მთელი საქართველოს მასშტაბით 200 ქალი
+              შეირჩა და 9 თვის განმავლობაში ხელოვნურ ინტელექტისა და
+              ვებ-დეველოპმენტის მიმართულებებით გადამზადა.
+            </p>
+          </div>
+          <div className="card3 card">
+            <img src={women3} alt="card photo" />
+            <p>
+              საქართველოში მცხოვრები უკრაინელი ქალები ტექნოლოგიებს შეისწავლიან.
+              პროექტს-BTU, გაეროს ქალთა ორგანიზაციისა და იაპონიის მთავრობის
+              მხარდაჭერით განახორციელებს.
+            </p>
+          </div>
+          <div className="card4 card">
+            <img src={women4} alt="card photo" />
+            <p>
+              ევროკავშირის მიერ მხარდაჭერილი პროექტი „ქალების მენტორობა ტექ
+              პროგრამებში’’ მიზნად ისახავს, სამი ნაკადის განმავლობაში, 1100
+              ბენეფიციარისა და 320 მენტორის მოზიდვას.
+            </p>
+          </div>
         </div>
       </div>
     </div>
